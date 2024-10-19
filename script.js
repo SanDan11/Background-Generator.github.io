1e1df7a74ea4540ac790a9b8bad00d95d5112398
Daniel <Danielrsanchez1990@gmail.com>
2024-10-18 21:06:36 -0700

    Search bars added

--- script.js
+++ script.js
@@ -8,29 +8,44 @@ function getColor() {
     return color;
 }
 
-function changeButtonColor(buttonId) {
-    const randomColor = getColor();
-    
-    const button = document.getElementById(buttonId);
-    
-    button.style.backgroundColor = randomColor;
 
-    button.textContent = "Color: " + randomColor;
+function isValidColor(color) {
+    const s = new Option().style;
+    s.color = color;
+    return s.color !== '';
 }
+
 
 function changeBodyBackground() {
+    const inputColor = document.getElementById("bodyColorInput").value;
     const randomColor = getColor();
-    document.body.style.backgroundColor = randomColor;
+    const colorToUse = isValidColor(inputColor) ? inputColor : randomColor;
 
-    const bodyColorDisplay = document.getElementById("body-color-display");
-    bodyColorDisplay.textContent = "Body Background Color: " + randomColor;
+    
+    document.body.style.backgroundColor = colorToUse;
+
+    const card1 = document.getElementById("card1");
+    card1.style.backgroundColor = colorToUse;
+
+    document.getElementById("color-display-1").textContent = "Card 1 Color: " + colorToUse;
 }
 
-function changeBackground(cardId, displayId) {
+function changeBackground(cardId, displayId, inputId) {
+    const inputColor = document.getElementById(inputId).value;
     const randomColor = getColor();
+    const colorToUse = isValidColor(inputColor) ? inputColor : randomColor;
+
     const card = document.getElementById(cardId);
-    card.style.backgroundColor = randomColor;
+    card.style.backgroundColor = colorToUse;
 
     const colorDisplay = document.getElementById(displayId);
-    colorDisplay.textContent = "Card Color: " + randomColor;
+    colorDisplay.textContent = "Card Color: " + colorToUse;
+}
+
+
+function changeButtonColor(buttonId) {
+    const randomColor = getColor();
+    const button = document.getElementById(buttonId);
+    button.style.backgroundColor = randomColor;
+    button.textContent = "Color: " + randomColor;
 }
